bcbca09dfe6ceaa328269a221790f8bc606ca4f4
Andybabic <andybabic93@gmail.com>
2022-04-20 12:31:44 +0200

    Update ModeSwitch.js

--- js/modules/ModeSwitch.js
+++ js/modules/ModeSwitch.js
@@ -23,55 +23,62 @@ import FilterableList from './FilterableList.js';
         };
          //METHODS
 
+         initModeSwitch(){
+             console.log(localStorage.overviewState);
+             if(localStorage.overviewState){
+                 //try to get state value from localstorage
+                 this.modules.filterableList = new FilterableList($("#filterableList"), localStorage.overviewState);
+                 this.vars.currentMode = localStorage.overviewState;
+             }else{
+                 //if no stored value -> prepared state as default
+                 this.modules.filterableList = new FilterableList($("#filterableList"), "prepare");
+             }
 
-initModeSwitch(){
-    console.log(localStorage.overviewState);
-    if(localStorage.overviewState){
-        //try to get state value from localstorage
-        this.modules.filterableList = new FilterableList($("#filterableList"), localStorage.overviewState);
-        this.vars.currentMode = localStorage.overviewState;
-    }else{
-        //if no stored value -> prepared state as default
-        this.modules.filterableList = new FilterableList($("#filterableList"), "prepare");
+             this.addInteraction();
+             this.addActiveClass();
+         };
+
+         addInteraction(){
+             this.doms.switchBtn.on("click", (e) => {
+                 const val = e.target.dataset.mode;
+                 //if the clicked value is different than the current state switch
+                 if(val != this.vars.currentMode){
+                     this.vars.currentMode = val;
+                     //store current state to localstorage
+                     localStorage.overviewState = val;
+                     this.switchMode();
+                     this.addActiveClass();
+                 }
+             });
+         }
+
+         addActiveClass(){
+             //remove active class first
+             $(".modeSwitch").removeClass("uk-active");
+             //add active class
+             $("#modeSwitch").find(`[data-mode='${this.vars.currentMode}']`).closest("li").addClass("uk-active");
+         }
+
+         switchMode(){
+             const mode = this.vars.currentMode;
+             switch (mode){
+                case "prepare":
+                    this.modules.filterableList.removeList();
+                    this.modules.filterableList = new FilterableList($("#filterableList"), "prepare");
+                    break;
+                case "return":
+                     this.modules.filterableList.removeList();
+                     this.modules.filterableList = new FilterableList($("#filterableList"), "return");
+                     break;
+
+             }
+         }
+
     }
-    
-
-    this.addInteraction();
-    this.addActiveClass();
-};
-
-addInteraction(){
-    this.doms.switchBtn.on("click", (e) => {
-        const val = e.target.dataset.mode;
-        //if the clicked value is different than the current state switch
-        if(val != this.vars.currentMode){
-            this.vars.currentMode = val;
-            //store current state to localstorage
-            localStorage.overviewState = val;
-            this.switchMode();
-            this.addActiveClass();
+    const init = () => {
+        if ($("#modeSwitch").length > 0 && $("#filterableList").length > 0) {
+            new ModeSwitch($("#modeSwitch"));
         }
-    });
-}
-
-addActiveClass(){
-    //remove active class first
-    $(".modeSwitch").removeClass("uk-active");
-    //add active class
-    $("#modeSwitch").find(`[data-mode='${this.vars.currentMode}']`).closest("li").addClass("uk-active");
-}
-
-switchMode(){
-    const mode = this.vars.currentMode;
-    switch (mode){
-        case "prepare":
-            this.modules.filterableList.removeList();
-            this.modules.filterableList = new FilterableList($("#filterableList"), "prepare");
-            break;
-        case "return":
-            this.modules.filterableList.removeList();
-            this.modules.filterableList = new FilterableList($("#filterableList"), "return");
-            break;
-       
-    }
-}
+    };
+    init();
+})(jQuery, window, document);
