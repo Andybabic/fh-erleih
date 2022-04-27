73555dd914224f8de712f9e4119d572be6b44ff5
unknown <natalie.fruehwirth@gmail.com>
2022-04-27 14:14:27 +0200

    Update Overview Page + Style of Settings Page

--- modules/filterableList.php
+++ modules/filterableList.php
@@ -1,12 +1,11 @@
 <?php
 ?>
 <script defer type="module" src="../js/modules/FilterableList.js"></script>
-<div id="filterableList">
+<div id="filterableList" >
     <div class="filterWrapper" >
 
 
     </div>
-    <div class="listWrapper">
-
+    <div class="listWrapper  uk-width-3-5@l uk-margin  uk-align-center">
     </div>
 </div>
