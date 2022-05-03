b2b3eb5aaee304806000fbacca7bedc53476526d
Natalie Fruehwirth <natalie.fruehwirth@gmail.com>
2022-05-03 10:26:06 +0200

    Updated Menu

--- js/classes/General.js
+++ js/classes/General.js
@@ -1,47 +1,46 @@
 "use strict";
-class General{
-    //CONSTRUCTOR
-    constructor() {
-        //code to run on every page without explicit call
-        console.log("gen0");
-        this.addDarkmode();
+class General {
+  //CONSTRUCTOR
+  constructor() {
+    //code to run on every page without explicit call
+    console.log("gen0");
+    this.addDarkmode();
+  }
+
+  //FORMAT - all functions that format something
+  formatName(string) {
+    //function to format Names with Umlaut
+    if (string == undefined) return "";
+    const badValues = {
+      "Ã¼": "ü",
+      "Ã-": "Ö",
+      Ãœ: "Ü",
+      "Ã¤": "ä",
+      "Ã¶": "ö",
+      ÃŸ: "ß",
+      Ã: "Ä",
     };
-
-    //FORMAT - all functions that format something
-    formatName(string){
-        //function to format Names with Umlaut
-        if(string == undefined) return "";
-        const badValues = {
-            "Ã¼": "ü",
-            "Ã-":"Ö",
-            "Ãœ":"Ü",
-            "Ã¤":"ä",
-            "Ã¶":"ö",
-            "ÃŸ":"ß",
-            "Ã":"Ä"
-        };
-        for (const key in badValues) {
-            string = string.replaceAll(key, badValues[key]);
-        }
-        return string;
+    for (const key in badValues) {
+      string = string.replaceAll(key, badValues[key]);
     }
+    return string;
+  }
 
-    formatDate(date){
-        //formats date object to rest api standard
-        const offset = date.getTimezoneOffset();
-        let newDate = new Date(date.getTime() - (offset*60*1000));
-        newDate = newDate.toISOString().split('T')[0];
+  formatDate(date) {
+    //formats date object to rest api standard
+    const offset = date.getTimezoneOffset();
+    let newDate = new Date(date.getTime() - offset * 60 * 1000);
+    newDate = newDate.toISOString().split("T")[0];
 
-        return newDate;
-    }
+    return newDate;
+  }
 
-
-    toggleLoader(DOMPos){
-        //toggles loader
-        if($(".loader-horizontal").length){
-            $(".loader-horizontal").slideToggle();
-        }else{
-            const loader = `
+  toggleLoader(DOMPos) {
+    //toggles loader
+    if ($(".loader-horizontal").length) {
+      $(".loader-horizontal").slideToggle();
+    } else {
+      const loader = `
                     <div class="loader-horizontal">
                       <div class="loader-horizontal__dot"></div>
                       <div class="loader-horizontal__dot"></div>
@@ -49,23 +48,38 @@ class General{
                     </div>
                 `;
 
-            $(loader).insertAfter(DOMPos);
-        }
+      $(loader).insertAfter(DOMPos);
     }
+  }
 
-    addDarkmode(){
-        //checks if darkmode is selected and adds it
-        if(localStorage.settings){
-            let settings = JSON.parse(localStorage.settings);
-            if(settings.darkMode){
-                $(":root").css("--colorBackgroundClean", "#212529");
-                $(":root").css("--colorBackgroundGrey", "#495057");
-                $(":root").css("--textColor", "#f8f9fa");
-            }
-        }
+  addDarkmode() {
+    //checks if darkmode is selected and adds it
+    if (localStorage.settings) {
+      let settings = JSON.parse(localStorage.settings);
+      if (settings.darkMode) {
+        $(":root").css("--colorBackgroundClean", "#212529");
+        $(":root").css("--colorBackgroundGrey", "#495057");
+        $(":root").css("--textColor", "#f8f9fa");
+      }
     }
+  }
 
+  openBurgerMenu() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
 
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".nav-link").forEach((n) =>
+      n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+      })
+    );
+  }
 }
 
 const general = new General();
--- modules/menu.php
+++ modules/menu.php
@@ -5,33 +5,70 @@ $domain = $_SERVER['HTTP_HOST'];
 ?>
 
 
-<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
-    <nav class="uk-navbar uk-navbar-container uk-margin">
+<!-- <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar"> -->
+    <!-- <nav class="uk-navbar uk-navbar-container uk-margin">
         <div class="uk-navbar-left">
 
 
             <a class="uk-navbar-toggle" href="#">
                 <span class="uk-margin-small-left">Username</span>
             </a>
-        </div>
+        </div> -->
 
-        <div class="uk-navbar-right">
-
-            <ul class="uk-navbar-nav" uk-navbar="mode:hover">
-                <li>
-                    <a href="#" uk-icon="icon: menu; ratio:2;"></a>
-                    <div class="uk-navbar-dropdown">
-                        <ul class="uk-nav uk-navbar-dropdown-nav">
-                            <li><a href="../pages/scan.php" class="uk-align-right">Eintragung</a></li>
-                            <li><a href="../pages/settings.php" class="uk-align-right">Einstellungen</a></li>
-                            <li><a href="<?php $domain ?>/functions/logout.php" class="uk-align-right"> Abmelden</a>
-                            </li>
-                        </ul>
-                    </div>
+      
+        <header>
+        <nav class="navbar">
+            <a  class="nav-branding"> Username</a>
+            <ul class="nav-menu">
+            <li class="nav-item">
+                    <a href="../pages/overview.php" class="nav-link">Übersicht</a>
+                </li>
+                <li class="nav-item">
+                    <a href="../pages/scan.php" class="nav-link">Eintragung</a>
+                </li>
+                           <li class="nav-item">
+                    <a href="../pages/settings.php"  class="nav-link">Einstellungen</a>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php $domain ?>/functions/logout.php"  class="nav-link">Abmelden</a>
                 </li>
             </ul>
-        </div>
+
+            <div class="hamburger">
+<span class="bar"></span>
+<span class="bar"></span>
+<span class="bar"></span>
+            </div>
+        </nav>
+    </header>
+
+    <script>
+ const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".nav-link").forEach((n) =>
+      n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+      })
+    );
+  
+    </script>
 
 
-    </nav>
-</div>
+
+
+         
+            
+    <!-- </nav>
+
+</div> -->
+
+
+<!-- </nav>
+</div> -->
