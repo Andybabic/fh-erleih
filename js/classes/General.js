7f4f5b62057d9612f4f6c0bff76b68882f53166a
unknown <natalie.fruehwirth@gmail.com>
2022-05-25 10:10:39 +0200

    hide/show filter buttons

--- js/classes/General.js
+++ js/classes/General.js
@@ -3,56 +3,58 @@ import Ajax from './Ajax.js';
 import NFCPopup from '../modules/NFCPopup.js';
 
 class General {
-  //CONSTRUCTOR
-  constructor() {
-    //code to run on every page without explicit call
-      window.ajax = new Ajax();
-      //applies darkmode to every page if selected
-      this.addDarkmode();
-      //burger button menu functionality
-      this.openBurgerMenu();
-      //checks on page load if there is an ID from NFC scan in localstorage
-      this.checkForNFCId();
-      //nfc listener
-      this.nfcListener();
-  }
-
-  //FORMAT - all functions that format something
-  formatName(string) {
-    //function to format Names with Umlaut
-    if (string == undefined) return "";
-    const badValues = {
-      "Ã¼": "ü",
-      "Ã-": "Ö",
-      Ãœ: "Ü",
-      "Ã¤": "ä",
-      "Ã¶": "ö",
-      ÃŸ: "ß",
-      Ã: "Ä",
-    };
-    for (const key in badValues) {
-      string = string.replaceAll(key, badValues[key]);
+    //CONSTRUCTOR
+    constructor() {
+        //code to run on every page without explicit call
+        window.ajax = new Ajax();
+        //applies darkmode to every page if selected
+        this.addDarkmode();
+        //burger button menu functionality
+        this.openBurgerMenu();
+        //checks on page load if there is an ID from NFC scan in localstorage
+        this.checkForNFCId();
+        //nfc listener
+        this.nfcListener();
+        //opens Div of Filter Buttons
+        this.openFilterButtonDiv();
     }
-    return string;
-  }
 
-  formatDate(date) {
-    //formats date object to rest api standard
-    const offset = date.getTimezoneOffset();
-    let newDate = new Date(date.getTime() - offset * 60 * 1000);
-    newDate = newDate.toISOString().split("T")[0];
+    //FORMAT - all functions that format something
+    formatName(string) {
+        //function to format Names with Umlaut
+        if (string == undefined) return "";
+        const badValues = {
+            "Ã¼": "ü",
+            "Ã-": "Ö",
+            Ãœ: "Ü",
+            "Ã¤": "ä",
+            "Ã¶": "ö",
+            ÃŸ: "ß",
+            Ã: "Ä",
+        };
+        for (const key in badValues) {
+            string = string.replaceAll(key, badValues[key]);
+        }
+        return string;
+    }
 
-    return newDate;
-  }
+    formatDate(date) {
+        //formats date object to rest api standard
+        const offset = date.getTimezoneOffset();
+        let newDate = new Date(date.getTime() - offset * 60 * 1000);
+        newDate = newDate.toISOString().split("T")[0];
+
+        return newDate;
+    }
 
 
-  //LOADER+
-  toggleLoader(DOMPos) {
-    //toggles loader
-    if ($(".loader-horizontal").length) {
-      $(".loader-horizontal").slideToggle();
-    } else {
-      const loader = `
+    //LOADER+
+    toggleLoader(DOMPos) {
+        //toggles loader
+        if ($(".loader-horizontal").length) {
+            $(".loader-horizontal").slideToggle();
+        } else {
+            const loader = `
                     <div class="loader-horizontal">
                       <div class="loader-horizontal__dot"></div>
                       <div class="loader-horizontal__dot"></div>
@@ -60,92 +62,113 @@ class General {
                     </div>
                 `;
 
-      $(loader).insertAfter(DOMPos);
+            $(loader).insertAfter(DOMPos);
+        }
     }
-  }
 
-  //DARKMODE
-  addDarkmode() {
-    //checks if darkmode is selected and adds it
-    if (localStorage.settings) {
-      let settings = JSON.parse(localStorage.settings);
-      if (settings.darkMode) {
-        $(":root").css("--colorBackgroundClean", "#212529");
-        $(":root").css("--colorBackgroundGrey", "#495057");
-        $(":root").css("--textColor", "#f8f9fa");
-      }
+    //DARKMODE
+    addDarkmode() {
+        //checks if darkmode is selected and adds it
+        if (localStorage.settings) {
+            let settings = JSON.parse(localStorage.settings);
+            if (settings.darkMode) {
+                $(":root").css("--colorBackgroundClean", "#212529");
+                $(":root").css("--colorBackgroundGrey", "#495057");
+                $(":root").css("--textColor", "#f8f9fa");
+            }
+        }
     }
-  }
 
-  //MENU
-  openBurgerMenu() {
-    const hamburger = document.querySelector(".hamburger");
-    const navMenu = document.querySelector(".nav-menu");
+    //MENU
+    openBurgerMenu() {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
 
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("active");
-      navMenu.classList.toggle("active");
-    });
+        hamburger.addEventListener("click", () => {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        });
 
-    document.querySelectorAll(".nav-link").forEach((n) =>
-      n.addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-      })
-    );
-  }
-
-  //PAGE REDIRECT
-  redirectWithPost(data) {
-    const url = "checklist.php";
-    const params = {
-      "data": JSON.stringify(data)
-    };
-    const form = document.createElement("form");
-    form.setAttribute("method", "post");
-    form.setAttribute("action", url);
-
-    for (let key in params) {
-      if (params.hasOwnProperty(key)) {
-        const hiddenField = document.createElement("input");
-        hiddenField.setAttribute("type", "hidden");
-        hiddenField.setAttribute("name", key);
-        hiddenField.setAttribute("value", params[key]);
-
-        form.appendChild(hiddenField);
-      }
+        document.querySelectorAll(".nav-link").forEach((n) =>
+            n.addEventListener("click", () => {
+                hamburger.classList.remove("active");
+                navMenu.classList.remove("active");
+            })
+        );
     }
-    document.body.appendChild(form);
-    form.submit();
-  }
 
-  //NFC
-  checkForNFCId(){
-    //checks at pageload if there is an ID from a NFC Scan in the localstorage
-    if(localStorage.nfcListener && localStorage.nfcListener != null){
-      console.log(localStorage.nfcListener);
-      this.createPopupFromScannedId(localStorage.nfcListener);
+    //PAGE REDIRECT
+    redirectWithPost(data) {
+        const url = "checklist.php";
+        const params = {
+            "data": JSON.stringify(data)
+        };
+        const form = document.createElement("form");
+        form.setAttribute("method", "post");
+        form.setAttribute("action", url);
+
+        for (let key in params) {
+            if (params.hasOwnProperty(key)) {
+                const hiddenField = document.createElement("input");
+                hiddenField.setAttribute("type", "hidden");
+                hiddenField.setAttribute("name", key);
+                hiddenField.setAttribute("value", params[key]);
+
+                form.appendChild(hiddenField);
+            }
+        }
+        document.body.appendChild(form);
+        form.submit();
     }
-  }
 
-  nfcListener(){
-    //listens to changes in localstorage
-    window.addEventListener('storage', (e) => {
-      if(e.key == "nfcListener" && e.newValue != null){
-        this.createPopupFromScannedId(e.newValue);
-      }
-    });
-  }
-
-  createPopupFromScannedId(id){
-    const eqId = parseInt(id);
-    if(!isNaN(eqId)){
-      localStorage.nfcListener = null;
-      new NFCPopup(eqId);
+    //NFC
+    checkForNFCId() {
+        //checks at pageload if there is an ID from a NFC Scan in the localstorage
+        if (localStorage.nfcListener && localStorage.nfcListener != null) {
+            console.log(localStorage.nfcListener);
+            this.createPopupFromScannedId(localStorage.nfcListener);
+        }
     }
-  }
 
+    nfcListener() {
+        //listens to changes in localstorage
+        window.addEventListener('storage', (e) => {
+            if (e.key == "nfcListener" && e.newValue != null) {
+                this.createPopupFromScannedId(e.newValue);
+            }
+        });
+    }
+
+    createPopupFromScannedId(id) {
+        const eqId = parseInt(id);
+        if (!isNaN(eqId)) {
+            localStorage.nfcListener = null;
+            new NFCPopup(eqId);
+        }
+    }
+
+    openFilterButtonDiv() {
+        var coll = document.getElementsByClassName("aufklappen");
+
+        $(".aufklappen").on("click", (e) => {
+            if ($(".filterWrapper").is(":visible")) {
+                $(".showFilterArrow").css({"transform": "rotate(180deg)"})
+                $(".aufklappen").css({"background-color": "var(--colorSecondary"})
+
+            } else {
+                $(".showFilterArrow").css({"transform": "rotate(0deg)"})
+                $(".aufklappen").css({"background-color": "var(--colorPrimary"})
+            }
+            $(".aufklappen").toggleClass("colorPrimary");
+            $(".filterWrapper").slideToggle();
+
+
+        });
+
+
+    }
 }
+
 
 window.general = new General();
 
--- modules/filterableList.php
+++ modules/filterableList.php
@@ -2,6 +2,10 @@
 ?>
 <script defer type="module" src="../js/modules/FilterableList.js"></script>
 <div id="filterableList" >
+    <button  class="aufklappen uk-button  colorPrimary  uk-align-center uk-flex uk-flex-between">Filter  <span class="showFilterArrow"><svg xmlns="http://www.w3.org/2000/svg" width="34.875" height="34.875" viewBox="0 0 34.875 34.875">
+                                  <path id="Icon_awesome-arrow-circle-down" data-name="Icon awesome-arrow-circle-down" d="M35.438,18A17.438,17.438,0,1,1,18,.563,17.434,17.434,0,0,1,35.438,18Zm-10.1-2.032L20.25,21.277V8.438A1.683,1.683,0,0,0,18.563,6.75H17.438A1.683,1.683,0,0,0,15.75,8.438V21.277l-5.091-5.309a1.689,1.689,0,0,0-2.412-.028l-.766.773a1.681,1.681,0,0,0,0,2.384l9.323,9.33a1.681,1.681,0,0,0,2.384,0l9.33-9.33a1.681,1.681,0,0,0,0-2.384l-.766-.773a1.689,1.689,0,0,0-2.412.028Z" transform="translate(-0.563 -0.563)"/>
+                                </svg></span></button>
+
     <div class="filterWrapper" >
 
 
@@ -9,3 +13,5 @@
     <div class="listWrapper  uk-width-3-5@l uk-margin  uk-align-center">
     </div>
 </div>
+
+
